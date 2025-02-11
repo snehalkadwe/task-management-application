aaa41a54d87cd8d5cc2453e7d783042b2ac7f595
snehalkadwe <snehalkadwe@gmail.com>
2025-02-12 01:02:56 +0530

    fix: configuration issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Events\TaskCompleted;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use App\Listeners\SendTaskCompletedNotification;
 
@@ -22,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
+
         Event::listen(
             TaskCompleted::class,
             SendTaskCompletedNotification::class,
